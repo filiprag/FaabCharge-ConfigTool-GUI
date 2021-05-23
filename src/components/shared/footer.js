e76ca10e1f711c1eef93a1f2d3a6edfb0dae16cb
Filip Ragnestål <filipragnestal@filips-mbp.home>
2021-05-23 15:38:59 +0200

    flexibel year i footer

--- src/components/shared/footer.js
+++ src/components/shared/footer.js
@@ -1,35 +1,39 @@
-import React from 'react';
-import './footer.css';
+import React, { useState, useEffect } from "react";
+import "./footer.css";
 import { Col, OverlayTrigger, Tooltip } from "react-bootstrap";
 import UploadIcon from "../../img/file-upload-solid.svg";
 
 function Footer() {
-    return (
-      <div className="footer">
-        <Col xs={6} md={8} lg={10} ClassName="ml-5">
-          <div class="text-center ml-5">
-            <a class=" mb-0 py-2 ml-5">©2021 Faabcharge All rights reserved.</a>
-          </div>
-        </Col>
-        <Col xs={1} md={1} lg={1} ClassName="m-0 m-auto">
-          <div className="footer-Icon">
-            <OverlayTrigger
-              overlay={
-                <Tooltip id="tooltip-disabled">Import a new CVS-file</Tooltip>
-              }
-            >
-              <a href="https://localhost:44345/Home">
-                <img
-                  class="mb-2"
-                  src={UploadIcon}
-                  style={{ width: "1.4rem", cursor: "pointer" }}
-                />
-              </a>
-            </OverlayTrigger>
-          </div>
-        </Col>
-      </div>
-    );
+  
+
+  return (
+    <div className="footer">
+      <Col xs={6} md={8} lg={10} ClassName="ml-5">
+        <div class="text-center ml-5">
+          <a class=" mb-0 py-2 ml-5">
+            &copy; Faabcharge {new Date().getFullYear()}. All Rights Reserved.
+          </a>
+        </div>
+      </Col>
+      <Col xs={1} md={1} lg={1} ClassName="m-0 m-auto">
+        <div className="footer-Icon">
+          <OverlayTrigger
+            overlay={
+              <Tooltip id="tooltip-disabled">Import a new CVS-file</Tooltip>
+            }
+          >
+            <a href="https://localhost:44345/Home">
+              <img
+                class="mb-2"
+                src={UploadIcon}
+                style={{ width: "1.4rem", cursor: "pointer" }}
+              />
+            </a>
+          </OverlayTrigger>
+        </div>
+      </Col>
+    </div>
+  );
 }
 
 export default Footer;
